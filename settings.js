50c18f5fb2b642a4f5f86f78a58345e46443a53d
hassanakbarbhh73 <Akbarf839@gmail.com>
2025-02-09 19:10:44 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Hassan BHH', // Your name
   ownerNumber: '923552342783', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
